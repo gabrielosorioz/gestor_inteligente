af68c53a30f9df27a9612012c3586b0d9e87c699
gabrielosorioz <gabrielosorio013@gmail.com>
2024-07-12 16:46:28 -0300

    build: add module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.gestor_inteligente {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.gabrielosorio.gestor_inteligente to javafx.fxml;
+    exports com.gabrielosorio.gestor_inteligente;
+    exports com.gabrielosorio.gestor_inteligente.model;
+    opens com.gabrielosorio.gestor_inteligente.model to javafx.fxml;
+    exports com.gabrielosorio.gestor_inteligente.model.enums;
+    opens com.gabrielosorio.gestor_inteligente.model.enums to javafx.fxml;
+}

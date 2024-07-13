7283398f5e18152e501cd623c8d420314f12880d
gabrielosorioz <gabrielosorio013@gmail.com>
2024-07-13 06:57:23 -0300

    style: change payment styles class
    This change improves the UI design for user experience with the Payment view

--- src/main/java/com/gabrielosorio/gestor_inteligente/PaymentViewController.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/PaymentViewController.java
@@ -2,6 +2,7 @@ package com.gabrielosorio.gestor_inteligente;
 
 import com.gabrielosorio.gestor_inteligente.model.Payment;
 import com.gabrielosorio.gestor_inteligente.model.enums.PaymentMethod;
+import com.gabrielosorio.gestor_inteligente.utils.TextFieldUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,7 +12,7 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.*;
 
-public class PDVController implements Initializable {
+public class PaymentViewController implements Initializable {
 
     @FXML
     private Button btnCredito,btnDebito,btnDinheiro,btnPix;
@@ -23,9 +24,13 @@ public class PDVController implements Initializable {
     private Label paymentLbl1,paymentLbl2,paymentLbl3,paymentLbl4;
 
     @FXML
+    private Label paymentCurrency1,paymentCurrency2,paymentCurrency3,paymentCurrency4;
+
+
+    @FXML
     private Label descontoLbl;
 
-    private List<Label> paymentLabels;
+    private List<Label> paymentLabels, paymentCurrencyLabels;
     private List<TextField> paymentFields;
     private List<Payment> payments;
     private Set<String> selectedPaymentTypes;
@@ -59,6 +64,7 @@ public class PDVController implements Initializable {
     private void loadLists(){
         paymentLabels = Arrays.asList(paymentLbl1,paymentLbl2,paymentLbl3,paymentLbl4);
         paymentFields = Arrays.asList(paymentField1,paymentField2,paymentField3,paymentField4);
+        paymentCurrencyLabels = Arrays.asList(paymentCurrency1,paymentCurrency2,paymentCurrency3,paymentCurrency4);
         selectedPaymentTypes = new HashSet<>();
         payments = new ArrayList<>();
     }
@@ -72,7 +78,7 @@ public class PDVController implements Initializable {
         }
 
         if(lastIndexField < paymentFields.size()){
-            showPaymentInput(btnPayment,paymentFields.get(lastIndexField),paymentLabels.get(lastIndexField));
+            showPaymentInput(btnPayment,paymentFields.get(lastIndexField),paymentLabels.get(lastIndexField),paymentCurrencyLabels.get(lastIndexField));
             selectedPaymentTypes.add(paymentType);
             payments.add(new Payment(method, 0.00));
             System.out.println(method.getDescription() + ": R$ " + payments.get(lastIndexField).getValue());
@@ -80,13 +86,16 @@ public class PDVController implements Initializable {
         }
     }
 
-    private void showPaymentInput(Button btnPayment,TextField paymentField, Label paymentLabel){
+    private void showPaymentInput(Button btnPayment,TextField paymentField, Label paymentLabel,Label currencyLabel){
         String paymentName = btnPayment.getText().trim();
         btnPayment.setStyle("-fx-border-color: #000000;");
         paymentLabel.setText(paymentName);
         paymentLabel.setVisible(true);
         paymentField.setVisible(true);
+        currencyLabel.setVisible(true);
+        TextFieldUtils.addPriceListener(paymentField);
         paymentField.requestFocus();
+        paymentField.positionCaret(paymentField.getText().length());
     }
 
     private void processPayment(){

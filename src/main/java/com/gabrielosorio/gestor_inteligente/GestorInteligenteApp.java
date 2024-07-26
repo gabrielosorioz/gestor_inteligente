93cfd2a577207f83b0af66e45643ed7cf8bc657b
gabrielosorioz <gabrielosorio013@gmail.com>
2024-07-26 19:00:55 -0300

    refactor: (main) comment unnecessary code

--- src/main/java/com/gabrielosorio/gestor_inteligente/GestorInteligenteApp.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/GestorInteligenteApp.java
@@ -2,18 +2,41 @@ package com.gabrielosorio.gestor_inteligente;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class GestorInteligenteApp extends Application {
+    double x,y = 0;
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GestorInteligenteApp.class.getResource("payment-view.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+//        Parent root = FXMLLoader.load(getClass().getResource("fxml/navigation_menu.fxml"));
+////        primaryStage.initStyle(StageStyle.UNDECORATED);
+//
+//        root.setOnMousePressed(event -> {
+//            x = event.getSceneX();
+//            y = event.getSceneY();
+//        });
+//
+//        root.setOnMouseDragged(event -> {
+//            primaryStage.setX(event.getScreenX() - x);
+//            primaryStage.setY(event.getScreenY() - y);
+//        });
+//
+//        primaryStage.setScene(new Scene(root, 700, 400));
+//        primaryStage.show();
+
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(GestorInteligenteApp.class.getResource("fxml/sale/main_navigation.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+//        primaryStage.setMaximized(true);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {

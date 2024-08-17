680859333d8959c2291c99f6d2158c62e09bf7db
gabrielosorioz <gabrielosorio013@gmail.com>
2024-08-17 12:37:29 -0300

    feat(model): implement category model class
    - Added `Category` class with properties for ID and description.
    - Included constructors, getters and setters for managing Category data.

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/model/Category.java
@@ -0,0 +1,28 @@
+package com.gabrielosorio.gestor_inteligente.model;
+
+public class Category {
+
+    private Integer id;
+    private String description;
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

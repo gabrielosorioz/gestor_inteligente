04e2560029800f28473d5c4c0afe5af234b699b0
gabrielosorioz <gabrielosorio013@gmail.com>
2024-08-23 13:56:53 -0300

    refactor: Enhance StockDataUtils with improved JSON handling and objects parsing.
    - **fetchStockData**: Simplified method to fetch and parse stock data from JSON file.
    - **parseStock**: Extracted and parsed `Stock` data from a `JSONObject`, included nested `Product` details.
    - **parseCategory**: Extracted `category` data from a `JSONObject`
    - **parseSupplier**: Extracted `Supplier` data from a `JSONObject`.
    - **parseProduct**: Extracted and constructed a `Product` object from JSONObject.
    - **putStockJSONObject**: updated `JSONObject` with `Stock` and nested `Product` , `Category` and `Supplier` data.
    - **findStockJSONObjectByProductId: Added method to find a `JSONObject`of `Stock` JSON ARRAY by productID.
    - **updateStock**: Integrated method to update stock data inthe JSON file based on product ID.
    
    - Improved error handling and logging for file operations and data parsing.

--- src/main/java/com/gabrielosorio/gestor_inteligente/utils/StockDataUtils.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/utils/StockDataUtils.java
@@ -7,92 +7,186 @@ import com.gabrielosorio.gestor_inteligente.model.Supplier;
 import com.gabrielosorio.gestor_inteligente.model.enums.Status;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.logging.Logger;
 
 public class StockDataUtils {
 
     private static final Logger log = Logger.getLogger(StockDataUtils.class.getName());
+    private static final String stockFilePath = "src/main/resources/com/gabrielosorio/gestor_inteligente/data/stock.json";
 
-    public static List<Stock> fetchStockData(){
-        String fileName = "src/main/resources/com/gabrielosorio/gestor_inteligente/data/products.json";
+    public static List<Stock> fetchStockData() {
+
+        JSONArray stockJSONArray = getJSONArray(stockFilePath);
         List<Stock> stockData = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            StringBuilder jsonString = new StringBuilder();
-            String line;
 
-            while((line = reader.readLine()) != null){
-                jsonString.append(line);
-            }
+        stockJSONArray.forEach(o -> {
+            JSONObject stockJSONObject = (JSONObject) o;
+            Stock stock = parseStock(stockJSONObject);
+            stockData.add(stock);
 
-            JSONArray jsonArray = new JSONArray(jsonString.toString());
+        });
 
-            jsonArray.forEach(e -> {
-                JSONObject productJsonObject = (JSONObject) e;
-
-                /** extract data from products */
-                Integer id = productJsonObject.getInt("id");
-                Integer productID = productJsonObject.getInt("productId");
-                String description = productJsonObject.getString("description");
-                String barCode = productJsonObject.getString("barCode");
-                BigDecimal costPrice = productJsonObject.getBigDecimal("costPrice");
-                BigDecimal sellingPrice = productJsonObject.getBigDecimal("sellingPrice");
-                Status status = Status.ACTIVE;
-                Timestamp now = new Timestamp(Calendar.getInstance().getTimeInMillis());
-
-                /** extract data from supplier */
-                JSONObject supplierJsonObject = productJsonObject.getJSONObject("supplier");
-                Integer supplierId = supplierJsonObject.getInt("id");
-                String supplierName = supplierJsonObject.getString("name");
-                Supplier supplier = new Supplier(supplierId,supplierName);
-
-                /** extract data from category */
-                JSONObject categoryJsonObject = productJsonObject.getJSONObject("category");
-                Integer categoryId = categoryJsonObject.getInt("id");
-                String categoryDescription = categoryJsonObject.getString("description");
-                Category category = new Category(categoryId,categoryDescription);
-
-
-                Product stockProduct = Product.builder()
-                        .id(id)
-                        .productId(productID)
-                        .barCode(barCode)
-                        .description(description)
-                        .costPrice(costPrice)
-                        .sellingPrice(sellingPrice)
-                        .supplier(supplier)
-                        .category(category)
-                        .status(status)
-                        .dateCreate(now)
-                        .dateUpdate(now)
-                        .dateDelete(null)
-                        .build();
-
-                Stock stock = new Stock(stockProduct);
-                stockData.add(stock);
-            });
-
-            return stockData;
-
-
-        } catch (FileNotFoundException e) {
-            log.severe("Error fetching stock data, file not founded: " + e.getMessage());
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            log.severe("Error fetching stock data: " + e.getMessage());
-            throw new RuntimeException(e);
-        }
-
+        return stockData;
 
     }
+
+    private static Stock parseStock(JSONObject stockJSONObject){
+        Integer id = stockJSONObject.getInt("id");
+        int quantity = stockJSONObject.getInt("quantity");
+        Timestamp lastUpdate =  Timestamp.valueOf(stockJSONObject.getString("lastUpdate"));
+
+        JSONObject productJSONObject =  stockJSONObject.getJSONObject("product");
+        Product product = parseProduct(productJSONObject);
+
+        Stock stock = new Stock(product,quantity);
+        stock.setId(id);
+        stock.setLastUpdate(lastUpdate);
+        return stock;
+    }
+
+    private static Category parseCategory(JSONObject categoryJSONObject){
+        Integer id = categoryJSONObject.getInt("id");
+        String description = categoryJSONObject.getString("description");
+        Category category = new Category(id,description);
+        return category;
+    }
+
+    private static Supplier parseSupplier(JSONObject supplierJSONObject){
+        Integer id = supplierJSONObject.getInt("id");
+        String name = supplierJSONObject.getString("name");
+        Supplier supplier = new Supplier(id,name);
+        return supplier;
+    }
+
+    private static Product parseProduct(JSONObject productJSONObject){
+
+        Integer id = productJSONObject.getInt("id");
+        Integer productID = productJSONObject.getInt("productId");
+        String description = productJSONObject.getString("description");
+        String barCode = productJSONObject.getString("barCode");
+        BigDecimal costPrice = productJSONObject.getBigDecimal("costPrice");
+        BigDecimal sellingPrice = productJSONObject.getBigDecimal("sellingPrice");
+        String lastUpdateString = productJSONObject.getString("dateUpdate");
+        String dateCreateString = productJSONObject.getString("dateCreate");
+        Timestamp lastUpdate = Timestamp.valueOf(lastUpdateString);
+        Timestamp dateCreate = Timestamp.valueOf(dateCreateString);
+        Status status = Status.ACTIVE;
+
+        /** extract data from supplier,category of product*/
+        JSONObject categoryJSONObject = productJSONObject.getJSONObject("category");
+        JSONObject supplierJSONObject = productJSONObject.getJSONObject("supplier");
+
+        Category category = parseCategory(categoryJSONObject);
+        Supplier supplier = parseSupplier(supplierJSONObject);
+
+        Product product = Product.builder()
+                .id(id)
+                .productId(productID)
+                .barCode(barCode)
+                .description(description)
+                .costPrice(costPrice)
+                .sellingPrice(sellingPrice)
+                .supplier(supplier)
+                .category(category)
+                .status(status)
+                .dateCreate(dateCreate)
+                .dateUpdate(lastUpdate)
+                .dateDelete(null)
+                .build();
+
+        return product;
+    }
+
+    private static JSONArray getJSONArray(String filename){
+        StringBuilder jsonContent = new StringBuilder();
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            while((line = reader.readLine()) != null){
+                jsonContent.append(line);
+            }
+            return new JSONArray(jsonContent.toString());
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void saveJSONToFile(String filename, JSONArray jsonArray){
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            writer.write(jsonArray.toString(4));
+        } catch (IOException e) {
+            log.severe("Error saving JSON to file: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void putStockJSONObject(JSONObject stockJSONObject, Stock stock){
+
+        Product stockProduct = stock.getProduct();
+
+        /** extract the data from stock and update */
+        stockJSONObject.put("id",stock.getId());
+        stockJSONObject.put("quantity",stock.getQuantity());
+        stockJSONObject.put("lastUpdate",stock.getLastUpdate());
+
+
+        /** extract data from product and put */
+        JSONObject productJSONObject = stockJSONObject.getJSONObject("product");
+        productJSONObject.put("id",stockProduct.getId());
+        productJSONObject.put("productId",stockProduct.getProductID());
+        productJSONObject.put("costPrice",stockProduct.getCostPrice().toPlainString());
+        productJSONObject.put("description",stockProduct.getDescription());
+        productJSONObject.put("dateCreate",stockProduct.getDateCreate());
+        productJSONObject.put("barCode",stockProduct.getBarCode());
+        productJSONObject.put("sellingPrice",stockProduct.getSellingPrice().toPlainString());
+        productJSONObject.put("markupPercent",stockProduct.getMarkupPercent());
+        productJSONObject.put("profitPercent",stockProduct.getProfitPercent());
+        productJSONObject.put("dateUpdate",stockProduct.getDateUpdate());
+
+        /**extract data from supplier of product and put */
+        JSONObject supplierJSONObject = productJSONObject.getJSONObject("supplier");
+        supplierJSONObject.put("id",stockProduct.getSupplier().getId());
+        supplierJSONObject.put("supplier",stockProduct.getSupplier().getName());
+
+        /**extract data from supplier of product and put */
+        JSONObject categoryJSONObject = productJSONObject.getJSONObject("category");
+        categoryJSONObject.put("id",stockProduct.getCategory().getId());
+        supplierJSONObject.put("supplier",stockProduct.getCategory().getDescription());
+
+    }
+
+    private static JSONObject findStockJSONObjectByProductId(JSONArray stockJSONArray, Integer productId){
+        for (Object o : stockJSONArray) {
+            JSONObject stockJSONObject = (JSONObject) o;
+            JSONObject productJSONObject = stockJSONObject.getJSONObject("product");
+
+            if (productJSONObject.getInt("productId") == productId) {
+                log.info("Product found. ");
+                final JSONObject stockJSONObjectFound = stockJSONObject;
+                return stockJSONObjectFound;
+            }
+        }
+        log.severe("Error searching for product, ID not found.");
+        throw new IllegalArgumentException("Invalid product ID to find Stock JSON object. " + "ID: " + productId);
+    }
+
+    public static void updateStock(Stock updatedStock){
+        JSONArray stockJSONArray = getJSONArray(stockFilePath);
+        JSONObject stockJSONObjectFound = findStockJSONObjectByProductId(stockJSONArray,updatedStock.getProduct().getProductID());
+        putStockJSONObject(stockJSONObjectFound,updatedStock);
+        saveJSONToFile(stockFilePath,stockJSONArray);
+    }
+
+
+
+
 
 }

a72dbdbc7b897e5cda373bfe13a8429f9fabd4c6
gabrielosorioz <gabrielosorio013@gmail.com>
2024-09-12 19:53:35 -0300

    feat(model): adds new attributes to supplier

--- src/main/java/com/gabrielosorio/gestor_inteligente/model/Supplier.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/model/Supplier.java
@@ -2,9 +2,48 @@ package com.gabrielosorio.gestor_inteligente.model;
 
 import com.gabrielosorio.gestor_inteligente.model.enums.PaymentMethod;
 
+import java.util.Optional;
+
 public class Supplier {
+
     private long id;
     private String name;
+    private Optional<String> CNPJ;
+    private Optional<String> address;
+    private Optional<String> cellPhone;
+    private Optional<String> email;
+
+    public Optional<String> getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(Optional<String> CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public Optional<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(Optional<String> address) {
+        this.address = address;
+    }
+
+    public Optional<String> getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(Optional<String> cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public Optional<String> getEmail() {
+        return email;
+    }
+
+    public void setEmail(Optional<String> email) {
+        this.email = email;
+    }
 
     public Supplier(long id, String name) {
         this.id = id;

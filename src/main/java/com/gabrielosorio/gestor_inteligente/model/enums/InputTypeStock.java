3aa69710fb07ee843f3923c03e008f0d354f0a85
gabrielosorioz <gabrielosorio013@gmail.com>
2024-09-13 19:02:20 -0300

    feat(model): add stock type enums
    - Add `InputTypeStock` enum for different types of stock inputs.
    - Add `OutputStockType` enum for various stock output types.

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/model/enums/InputTypeStock.java
@@ -0,0 +1,8 @@
+package com.gabrielosorio.gestor_inteligente.model.enums;
+
+public enum InputTypeStock  {
+    DEVOLUCAO_CLIENTE,
+    COMPRAS,
+    TRANSFERENCIA_ENTRE_LOJAS,
+    AJUSTE_DE_ESTOQUE
+}
---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/model/enums/OutputStockType.java
@@ -0,0 +1,9 @@
+package com.gabrielosorio.gestor_inteligente.model.enums;
+
+public enum OutputStockType {
+    DEVOLUCAO_AO_FORNECEDOR,
+    TRANSFERENCIA_ENTRE_LOJAS,
+    USO_E_CONSUMO_INTERNO,
+    AJUSTE_DE_ESTOQUE,
+    OUTRAS_SAIDAS
+}

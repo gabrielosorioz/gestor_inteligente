f8de6e3cf3a0bddb7ec3b2e86b45b8142d2d4bd6
gabrielosorioz <gabrielosorio013@gmail.com>
2024-09-13 18:25:16 -0300

    feat(repository): implement repository strategy pattern
    - Added RepositoryStrategy interface to define operations for repositories.
    - Updated Repository class to use RepositoryStrategy for CRUD operations

--- src/main/java/com/gabrielosorio/gestor_inteligente/repository/Repository.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/repository/Repository.java
@@ -1,14 +1,38 @@
 package com.gabrielosorio.gestor_inteligente.repository;
 
 import com.gabrielosorio.gestor_inteligente.repository.specification.Specification;
-
-import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
-public interface Repository <T> {
-    void add(T t);
-    List<T> getAll();
-    List<T> findBySpecification(Specification<T> specification);
-    void update(T oldT, T newT);
-    void remove(long id);
+public abstract class Repository <T> {
+
+    private RepositoryStrategy<T> strategy;
+
+    public void init(RepositoryStrategy<T> strategy){
+        this.strategy = strategy;
+    }
+
+    public T add(T entity){
+        return strategy.add(entity);
+    }
+
+    public Optional<T> find(long id){
+        return strategy.find(id);
+    }
+
+    public List<T> findAll(){
+        return strategy.findAll();
+    }
+
+    public List<T> findBySpecification(Specification<T> specification){
+        return strategy.findBySpecification(specification);
+    }
+
+    public T update(T newT){
+        return strategy.update(newT);
+    }
+
+    public boolean remove(long id){
+        return strategy.remove(id);
+    }
 }

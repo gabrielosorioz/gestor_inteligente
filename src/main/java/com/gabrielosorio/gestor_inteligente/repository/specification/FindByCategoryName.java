b950321eb568b487f54c830afc60c5dcbf64f917
gabrielosorioz <gabrielosorio013@gmail.com>
2024-09-17 12:11:29 -0300

    feat(repository): add PostgreSQL strategies for Category
    - Add `FindByCategoryName` specification to search categories by name.
    - Implement `PSQLCategoryStrategy` with methods for CRUD operations and filtering by specification for Category.
    
    Includes methods for adding, finding, updating, and deleting categories, and integrates with QueryLoader and ConnectionFactory configurations.

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/repository/specification/FindByCategoryName.java
@@ -0,0 +1,35 @@
+package com.gabrielosorio.gestor_inteligente.repository.specification;
+
+import com.gabrielosorio.gestor_inteligente.config.DBScheme;
+import com.gabrielosorio.gestor_inteligente.config.QueryLoader;
+import com.gabrielosorio.gestor_inteligente.model.Category;
+
+import java.util.List;
+
+public class FindByCategoryName implements Specification<Category> {
+
+    private final QueryLoader qLoader;
+    private final String description;
+
+    public FindByCategoryName(String description) {
+        this.qLoader = new QueryLoader(DBScheme.POSTGRESQL);
+        this.description = description;
+    }
+
+
+    @Override
+    public String toSql() {
+        return qLoader.getQuery("findCategoryByName");
+    }
+
+    @Override
+    public List<Object> getParameters() {
+        return List.of(description);
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Category category) {
+        return description.equalsIgnoreCase(category.getDescription());
+    }
+
+}

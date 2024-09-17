66aec220f6147b4de3615799aa9d95bead4559cc
gabrielosorioz <gabrielosorio013@gmail.com>
2024-09-17 12:05:23 -0300

    feat(db): add QueryLoader utility and PostgreSQL queries
    - Introduced QueryLoader class for loading database queries from properties files.
    - Added PostgreSQL queries for product and category operations.

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/config/QueryLoader.java
@@ -0,0 +1,31 @@
+package com.gabrielosorio.gestor_inteligente.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+public class QueryLoader {
+
+    private Properties queries = new Properties();
+    private final String dbPrefix;
+    private Logger log = Logger.getLogger(getClass().getName());
+
+    public QueryLoader(DBScheme database) {
+        this.dbPrefix = database.getPrefix() + ".";
+        try(InputStream input = getClass().getClassLoader().getResourceAsStream("db/queries.properties")) {
+            if(input == null){
+                throw new FileNotFoundException("Queries file not found!.");
+            }
+            queries.load(input);
+        } catch (IOException e) {
+            log.severe("Error loading query file " + e.getLocalizedMessage() + " " + e.getCause());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getQuery(String queryKey){
+        return queries.getProperty(dbPrefix + queryKey);
+    }
+}

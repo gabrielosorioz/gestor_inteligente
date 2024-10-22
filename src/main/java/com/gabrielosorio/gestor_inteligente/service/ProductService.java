c6e87dc82134a219ad2bdd7adaa8f3feec66b5e8
gabrielosorioz <gabrielosorio013@gmail.com>
2024-10-22 20:16:25 -0300

    refactor(service): adds new feature to service
    - throws product exception if its code is present in
    the database.
    
    - adds method which update the product and update `last update time`

--- src/main/java/com/gabrielosorio/gestor_inteligente/service/ProductService.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/service/ProductService.java
@@ -1,8 +1,9 @@
 package com.gabrielosorio.gestor_inteligente.service;
-
+import com.gabrielosorio.gestor_inteligente.exception.ProductException;
 import com.gabrielosorio.gestor_inteligente.model.Product;
 import com.gabrielosorio.gestor_inteligente.repository.ProductRepository;
-import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 public class ProductService {
 
@@ -12,8 +13,7 @@ public class ProductService {
         this.pRepository = pRepository;
     }
 
-    public void save(Product product) throws SQLException {
-
+    public void save(Product product) throws ProductException {
         long pCode;
 
         if(product.getProductCode() == 0){
@@ -22,12 +22,22 @@ public class ProductService {
             pCode = product.getProductCode();
 
             if(pRepository.existPCode(product.getProductCode())){
-                throw new SQLException("The product code already exists. ");
+                throw new ProductException("The product code already exists. ");
+            }
+
+            if(pRepository.existsBarCode(product.getBarCode().orElse(""))){
+                throw new ProductException("The product bar code already exists. ");
             }
         }
 
         product.setProductCode(pCode);
+        product.setDateUpdate(Timestamp.from(Instant.now()));
         pRepository.add(product);
+    }
+
+    public void update(Product product) {
+        product.setDateUpdate(Timestamp.from(Instant.now()));
+        pRepository.update(product);
     }
 
 

a7aa5537bca7772e4aa2c6f15105fc9c73d8719d
gabrielosorioz <gabrielosorio013@gmail.com>
2024-10-28 11:50:09 -0300

    feat(category): adds repository layer to Category

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.gabrielosorio.gestor_inteligente.repository;
+
+import com.gabrielosorio.gestor_inteligente.model.Category;
+
+public class CategoryRepository extends Repository<Category> {
+}

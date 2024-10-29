453d7d37de6940ea0b486a47034c5af797865437
gabrielosorioz <gabrielosorio013@gmail.com>
2024-10-29 10:26:58 -0300

    feat(repository): adds `SaleProduct` repository

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/repository/SaleProductRepository.java
@@ -0,0 +1,7 @@
+package com.gabrielosorio.gestor_inteligente.repository;
+
+import com.gabrielosorio.gestor_inteligente.model.SaleProduct;
+
+public class SaleProductRepository extends Repository<SaleProduct> {
+
+}

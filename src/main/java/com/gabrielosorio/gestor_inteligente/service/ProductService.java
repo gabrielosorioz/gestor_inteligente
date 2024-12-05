fb3b052df9f16c71648f3703e26c84a1ec565a1b
gabrielosorioz <gabrielosorio013@gmail.com>
2024-12-05 16:50:52 -0300

    refactor(service): introduces the `ProductDataContext`
    - The product service layer now operates product data based on the data context, ensuring that all products are shared with the global access point

--- src/main/java/com/gabrielosorio/gestor_inteligente/service/ProductService.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/service/ProductService.java
@@ -2,7 +2,13 @@ package com.gabrielosorio.gestor_inteligente.service;
 
 import com.gabrielosorio.gestor_inteligente.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     void save(Product product);
     void update(Product product);
+    List<Product> findAllProducts();
+    void increaseQuantity(long id, long quantity);
+    void decreaseQuantity(long id, long quantity);
+
 }
--- src/main/java/com/gabrielosorio/gestor_inteligente/service/impl/ProductServiceImpl.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/service/impl/ProductServiceImpl.java
@@ -1,4 +1,6 @@
 package com.gabrielosorio.gestor_inteligente.service.impl;
+import com.gabrielosorio.gestor_inteligente.Inject;
+import com.gabrielosorio.gestor_inteligente.datacontext.ProductDataContext;
 import com.gabrielosorio.gestor_inteligente.exception.ProductException;
 import com.gabrielosorio.gestor_inteligente.model.Product;
 import com.gabrielosorio.gestor_inteligente.repository.ProductRepository;
@@ -6,13 +8,17 @@ import com.gabrielosorio.gestor_inteligente.service.ProductService;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository pRepository;
+    private final ProductDataContext prodDataContext;
 
     public ProductServiceImpl(ProductRepository pRepository) {
         this.pRepository = pRepository;
+        this.prodDataContext = ProductDataContext.getInstance(pRepository);
     }
 
     @Override
@@ -35,16 +41,58 @@ public class ProductServiceImpl implements ProductService {
 
         product.setProductCode(pCode);
         product.setDateUpdate(Timestamp.from(Instant.now()));
-        pRepository.add(product);
+        prodDataContext.add(product);
+    }
+
+    @Override
+    public List<Product> findAllProducts(){
+        return prodDataContext.findAll();
     }
 
     @Override
     public void update(Product product) {
         product.setDateUpdate(Timestamp.from(Instant.now()));
-        pRepository.update(product);
+        prodDataContext.update(product);
     }
 
+    public void increaseQuantity(long id, long quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("The quantity must be greater than zero.");
+        }
+        var product = prodDataContext.find(id);
 
+        product.ifPresentOrElse(
+                prod -> {
+                    long currentQuantity = prod.getQuantity();
+                    prod.setQuantity(currentQuantity + quantity);
+                    prodDataContext.update(prod);
+                },
+                () -> {
+                    throw new IllegalArgumentException("Product with ID " + id + " not found.");
+                }
+        );
+    }
 
+    public void decreaseQuantity(long id, long quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("The quantity must be greater than zero.");
+        }
+        var product = prodDataContext.find(id);
 
+        product.ifPresentOrElse(
+                prod -> {
+                    long currentQuantity = prod.getQuantity();
+                    if (currentQuantity < quantity) {
+                        throw new IllegalArgumentException(
+                                "Insufficient stock for product with ID " + id + ". Current stock: " + currentQuantity);
+                    }
+
+                    prod.setQuantity(currentQuantity - quantity);
+                    prodDataContext.update(prod);
+                },
+                () -> {
+                    throw new IllegalArgumentException("Product with ID" + id + " not found.");
+                }
+        );
+    }
 }

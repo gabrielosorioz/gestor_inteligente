f685d3431f1389c9d2b21ba86f22599a91afbf90
gabrielosorioz <gabrielosorio013@gmail.com>
2025-01-06 18:19:47 -0300

    feat: create Permission class
    - Added Permission class with fields for id, permission type, and description.

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/model/Permission.java
@@ -0,0 +1,9 @@
+package com.gabrielosorio.gestor_inteligente.model;
+
+import com.gabrielosorio.gestor_inteligente.model.enums.PermissionType;
+
+public class Permission {
+    private long id;
+    private PermissionType permissionType;
+    private String description;
+}

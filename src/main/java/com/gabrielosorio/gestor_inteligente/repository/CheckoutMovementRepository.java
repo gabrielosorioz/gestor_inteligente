26505b92c663b3aa205594365b114800bbfc50e5
gabrielosorioz <gabrielosorio013@gmail.com>
2025-01-06 18:39:03 -0300

    feat(repository): create CheckoutMovementRepository for managing checkout movements
    - Added `CheckoutMovementRepository` extending the base `Repository` class.
    - Provides data access functionality for `CheckoutMovement` model.

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/repository/CheckoutMovementRepository.java
@@ -0,0 +1,6 @@
+package com.gabrielosorio.gestor_inteligente.repository;
+
+import com.gabrielosorio.gestor_inteligente.model.CheckoutMovement;
+
+public class CheckoutMovementRepository extends Repository<CheckoutMovement>{
+}

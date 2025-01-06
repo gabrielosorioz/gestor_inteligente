38b7fa951500cceb23be8121e6f6427f9b526e58
gabrielosorioz <gabrielosorio013@gmail.com>
2025-01-06 18:32:08 -0300

    feat: implement specification to find open checkout for today
    - Created `FindOpenCheckoutForToday` specification to query for open checkouts.
    - Retrieves checkouts that are opened today and still open (not closed).
    - Utilizes `QueryLoader` to fetch the SQL query and applies parameters for status and today's date.
    - Ensures that the checkout is still open by checking for `null` in the `closedAt` field.

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/repository/specification/FindOpenCheckoutForToday.java
@@ -0,0 +1,35 @@
+package com.gabrielosorio.gestor_inteligente.repository.specification;
+import com.gabrielosorio.gestor_inteligente.config.DBScheme;
+import com.gabrielosorio.gestor_inteligente.config.QueryLoader;
+import com.gabrielosorio.gestor_inteligente.model.Checkout;
+import com.gabrielosorio.gestor_inteligente.model.enums.CheckoutStatus;
+import java.time.LocalDate;
+import java.util.List;
+
+public class FindOpenCheckoutForToday implements Specification<Checkout> {
+
+    private final LocalDate today;
+    private final QueryLoader qLoader;
+
+    public FindOpenCheckoutForToday() {
+        this.today = LocalDate.now();
+        this.qLoader = new QueryLoader(DBScheme.POSTGRESQL);
+    }
+
+    @Override
+    public String toSql() {
+        return qLoader.getQuery("findOpenCheckoutForToday");
+    }
+
+    @Override
+    public List<Object> getParameters() {
+        return List.of(CheckoutStatus.OPEN.getName(), today);
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Checkout checkout) {
+        return checkout.getStatus() == CheckoutStatus.OPEN &&
+                checkout.getOpenedAt().toLocalDate().isEqual(today) &&
+                checkout.getClosedAt() == null;
+    }
+}

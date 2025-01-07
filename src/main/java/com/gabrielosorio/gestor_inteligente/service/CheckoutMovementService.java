48b15518c6deec45f221e5cabd268475b618e509
gabrielosorioz <gabrielosorio013@gmail.com>
2025-01-07 13:12:37 -0300

    feat: add saveAll method for saving multiple CheckoutMovement objects

--- src/main/java/com/gabrielosorio/gestor_inteligente/service/CheckoutMovementService.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/service/CheckoutMovementService.java
@@ -2,12 +2,15 @@ package com.gabrielosorio.gestor_inteligente.service;
 import com.gabrielosorio.gestor_inteligente.model.Checkout;
 import com.gabrielosorio.gestor_inteligente.model.CheckoutMovement;
 import com.gabrielosorio.gestor_inteligente.model.Payment;
+import com.gabrielosorio.gestor_inteligente.model.SaleProduct;
 import com.gabrielosorio.gestor_inteligente.model.enums.TypeCheckoutMovement;
+
+import java.util.List;
 
 
 public interface CheckoutMovementService {
 
     CheckoutMovement addMovement(CheckoutMovement movement);
-
+    List<CheckoutMovement> saveAll(List<CheckoutMovement> checkoutMovements);
     CheckoutMovement buildCheckoutMovement(Checkout checkout, Payment payment, String obs, TypeCheckoutMovement type);
 }

60194dc8884edd47b02ac9ae0a75b2aaf102e2a1
gabrielosorioz <gabrielosorio013@gmail.com>
2025-01-07 13:12:47 -0300

    feat: add saveAll method for saving multiple CheckoutMovement objects

--- src/main/java/com/gabrielosorio/gestor_inteligente/service/impl/CheckoutMovementServiceImpl.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/service/impl/CheckoutMovementServiceImpl.java
@@ -3,10 +3,12 @@ package com.gabrielosorio.gestor_inteligente.service.impl;
 import com.gabrielosorio.gestor_inteligente.model.Checkout;
 import com.gabrielosorio.gestor_inteligente.model.CheckoutMovement;
 import com.gabrielosorio.gestor_inteligente.model.Payment;
+import com.gabrielosorio.gestor_inteligente.model.SaleProduct;
 import com.gabrielosorio.gestor_inteligente.model.enums.TypeCheckoutMovement;
 import com.gabrielosorio.gestor_inteligente.repository.CheckoutMovementRepository;
 import com.gabrielosorio.gestor_inteligente.service.CheckoutMovementService;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class CheckoutMovementServiceImpl implements CheckoutMovementService {
 
@@ -17,10 +19,16 @@ public class CheckoutMovementServiceImpl implements CheckoutMovementService {
     }
 
     @Override
+    public List<CheckoutMovement> saveAll(List<CheckoutMovement> checkoutMovements) {
+        return checkoutMovementRepository.addAll(checkoutMovements);
+    }
+
+    @Override
     public CheckoutMovement addMovement(CheckoutMovement movement) {
         movement.setDateTime(LocalDateTime.now());
         return checkoutMovementRepository.add(movement);
     }
+
 
     @Override
     public CheckoutMovement buildCheckoutMovement(Checkout checkout, Payment payment, String obs, TypeCheckoutMovement type) {

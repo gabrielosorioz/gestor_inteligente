bfb027ec2947c296ab586decdd94c0a586c93b5a
gabrielosorioz <gabrielosorio013@gmail.com>
2025-01-07 13:09:57 -0300

    feat: add Permission class with id, type, description, and name fields
    - Introduced `Permission` class to represent permissions with an ID, type, description, and name.
    - Added fields for `permissionType` (enum) and `description` to define the characteristics of a permission.

--- src/main/java/com/gabrielosorio/gestor_inteligente/model/Permission.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/model/Permission.java
@@ -5,5 +5,29 @@ import com.gabrielosorio.gestor_inteligente.model.enums.PermissionType;
 public class Permission {
     private long id;
     private PermissionType permissionType;
-    private String description;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public PermissionType getPermissionType() {
+        return permissionType;
+    }
+
+    public void setPermissionType(PermissionType permissionType) {
+        this.permissionType = permissionType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

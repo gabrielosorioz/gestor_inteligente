72025273c760042ca68eb864650b7ef0b49e3d1d
gabrielosorioz <gabrielosorio013@gmail.com>
2025-04-03 18:04:38 -0300

    feat(service): cria um service factory

---
+++ src/main/java/com/gabrielosorio/gestor_inteligente/service/impl/ServiceFactory.java
@@ -0,0 +1,88 @@
+package com.gabrielosorio.gestor_inteligente.service.impl;
+import com.gabrielosorio.gestor_inteligente.repository.base.RepositoryFactory;
+import com.gabrielosorio.gestor_inteligente.service.base.*;
+
+/**
+ * Factory responsible for creating and managing instance of application services.
+ * It centralizes the creation of all services and manages their dependencies
+ */
+public class ServiceFactory {
+
+    private final RepositoryFactory repositoryFactory;
+    private SaleService saleService;
+    private SaleProductService saleProductService;
+    private SalePaymentService salePaymentService;
+    private ProductService productService;
+    private CheckoutMovementService checkoutMovementService;
+    private CheckoutService checkoutService;
+    private SaleCheckoutMovementService saleCheckoutMovementService;
+
+    /**
+     * Constructor that receives a repository factory necessary to create the services.
+     * @param repositoryFactory factory que fornece acesso aos repositórios
+     */
+    public ServiceFactory(RepositoryFactory repositoryFactory) {
+        this.repositoryFactory = repositoryFactory;
+    }
+
+    public ProductService getProductService() {
+        if (productService == null) {
+            productService = new ProductServiceImpl(repositoryFactory.getProductRepository());
+        }
+        return productService;
+    }
+
+    public SaleProductService getSaleProductService() {
+        if (saleProductService == null) {
+            saleProductService = new SaleProductServiceImpl(repositoryFactory.getSaleProductRepository());
+        }
+        return saleProductService;
+    }
+
+    public SalePaymentService getSalePaymentService() {
+        if (salePaymentService == null) {
+            salePaymentService = new SalePaymentServiceImpl(repositoryFactory.getSalePaymentRepository());
+        }
+        return salePaymentService;
+    }
+
+    public CheckoutMovementService getCheckoutMovementService() {
+        if (checkoutMovementService == null) {
+            checkoutMovementService = new CheckoutMovementServiceImpl(
+                    repositoryFactory.getCheckoutMovementRepository());
+        }
+        return checkoutMovementService;
+    }
+
+    public CheckoutService getCheckoutService() {
+        if (checkoutService == null) {
+            checkoutService = new CheckoutServiceImpl(
+                    repositoryFactory.getCheckoutRepository(),
+                    getCheckoutMovementService());
+        }
+        return checkoutService;
+    }
+
+    public SaleCheckoutMovementService getSaleCheckoutMovementService() {
+        if (saleCheckoutMovementService == null) {
+            saleCheckoutMovementService = new SaleCheckoutMovementServiceImpl(
+                    repositoryFactory.getSaleCheckoutMovementRepository());
+        }
+        return saleCheckoutMovementService;
+    }
+
+    public SaleService getSaleService() {
+        if (saleService == null) {
+            saleService = new SaleServiceImpl(
+                    repositoryFactory.getSaleRepository(),
+                    getSaleProductService(),
+                    getSalePaymentService(),
+                    getCheckoutMovementService(),
+                    getCheckoutService(),
+                    getProductService(),
+                    getSaleCheckoutMovementService()
+            );
+        }
+        return saleService;
+    }
+}

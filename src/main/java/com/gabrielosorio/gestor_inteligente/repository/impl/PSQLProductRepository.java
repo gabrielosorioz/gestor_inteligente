cd20143d2829073f23ee6001e53e742bfb0f9845
gabrielosorioz <gabrielosorio013@gmail.com>
2025-04-04 11:25:32 -0300

    feat: implement PSQLProductStrategy checks in PSQLProductRepository
    - Added type-specific logic for `existsPCode`, `existsBarCode`, and `genPCode` methods using `PSQLProductStrategy`.
    - Integrated `PSQLProductStrategy` to handle PostgreSQL-specific operations.

--- src/main/java/com/gabrielosorio/gestor_inteligente/repository/impl/PSQLProductRepository.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/repository/impl/PSQLProductRepository.java
@@ -5,6 +5,7 @@ import com.gabrielosorio.gestor_inteligente.repository.base.ProductRepository;
 import com.gabrielosorio.gestor_inteligente.repository.base.Repository;
 import com.gabrielosorio.gestor_inteligente.repository.strategy.base.ProductRepositoryStrategy;
 import com.gabrielosorio.gestor_inteligente.repository.strategy.base.RepositoryStrategy;
+import com.gabrielosorio.gestor_inteligente.repository.strategy.psql.PSQLProductStrategy;
 
 public class PSQLProductRepository extends Repository<Product> implements ProductRepository {
 
@@ -12,19 +13,27 @@ public class PSQLProductRepository extends Repository<Product> implements Produc
         super.init(strategy);
     }
 
-
     @Override
     public boolean existsPCode(long pCode) {
+        if(strategy instanceof PSQLProductStrategy psqlProductStrategy) {
+            return psqlProductStrategy.existsPCode(pCode);
+        }
         return false;
     }
 
     @Override
     public boolean existsBarCode(String barCode) {
+        if(strategy instanceof PSQLProductStrategy psqlProductStrategy) {
+            return psqlProductStrategy.existsBarCode(barCode);
+        }
         return false;
     }
 
     @Override
     public long genPCode() {
+        if(strategy instanceof PSQLProductStrategy psqlProductStrategy) {
+            return psqlProductStrategy.genPCode();
+        }
         return 0;
     }
 }

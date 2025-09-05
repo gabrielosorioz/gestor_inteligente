f19b75249c2fdf169544cd252ee9a832a95d38ae
gabrielosorioz <gabrielosorio013@gmail.com>
2025-09-05 13:27:36 -0300

    refactor(app): load login via screen loader service

--- src/main/java/com/gabrielosorio/gestor_inteligente/GestorInteligenteApp.java
+++ src/main/java/com/gabrielosorio/gestor_inteligente/GestorInteligenteApp.java
@@ -1,8 +1,12 @@
 package com.gabrielosorio.gestor_inteligente;
 
+import com.gabrielosorio.gestor_inteligente.config.ConnectionFactory;
+import com.gabrielosorio.gestor_inteligente.config.DBScheme;
+import com.gabrielosorio.gestor_inteligente.repository.factory.PSQLRepositoryFactory;
+import com.gabrielosorio.gestor_inteligente.service.base.ScreenLoaderService;
+import com.gabrielosorio.gestor_inteligente.service.impl.ServiceFactory;
+import com.gabrielosorio.gestor_inteligente.service.view.ScreenLoaderServiceImpl;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,15 +15,17 @@ import java.io.IOException;
 public class GestorInteligenteApp extends Application {
     double x,y = 0;
     private static Stage pStage;
+    PSQLRepositoryFactory psqlRepositoryFactory = new PSQLRepositoryFactory(ConnectionFactory.getInstance(DBScheme.POSTGRESQL));
+    private ServiceFactory serviceFactory = new ServiceFactory(psqlRepositoryFactory);
+    private ScreenLoaderService screenLoaderService = new ScreenLoaderServiceImpl(serviceFactory);;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GestorInteligenteApp.class.getResource("fxml/MainNavigation.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        primaryStage.setScene(scene);
+        pStage = primaryStage;
+        screenLoaderService.loadLogin(primaryStage);
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
-        pStage = primaryStage;
+
     }
 
     public static Stage getPrimaryStage(){
